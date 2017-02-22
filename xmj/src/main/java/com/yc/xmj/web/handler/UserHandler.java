c3cc01ebf9bc32df76ca113bd0dbada93021a854
xiaodan'sFather <xiaodan'sFather@enshide.com>
2017-02-22 11:18:31 +0800

    添加了短信jar包 删除了com.yc.xmj.handder包，将其中的类转移到了 com.yc.xmj.web.handder中

--- xmj/src/main/java/com/yc/xmj/web/handler/UserHandler.java
+++ xmj/src/main/java/com/yc/xmj/web/handler/UserHandler.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.yc.xmj.entity.User;
 import com.yc.xmj.service.UserService;
+import com.yc.xmj.util.SMS;
 
 
 @Controller
@@ -19,13 +21,38 @@ public class UserHandler {
 	private UserService userService;
 	
 	@RequestMapping("/login")
-	public String login(User user,ModelMap map) {
-		if(user != null){
+	@ResponseBody
+	public boolean login(User user,ModelMap map) {
+		if(user.getU_id() > 0){
 			System.out.println(user);
-			map.put("manageName", user);
-			return "redirect:/index.jsp";
+			map.put("userName", user);
+			/*return "redirect:/index.jsp";*/
+			return true;
 		}
 		map.put("errorMsg", "用户名或密码错误");
-		return "forward:/login.jsp";
+		/*return "forward:/login.jsp";*/
+		return false;
+	}
+	
+	@RequestMapping("/register")
+	@ResponseBody
+	public boolean register(User user,ModelMap map) {
+		System.out.println(user);
+		if(userService.register(user)){
+			/*return "redirect:/page/login.jsp";*/
+			return true;
+		}
+		/*return "forward:/register.jsp";*/
+		return false;
+	}
+	
+	@RequestMapping("/getSMS")
+	@ResponseBody
+	public String getSMS(User user,ModelMap map) {
+		System.out.println(user);
+		SMS sms = new SMS();
+		sms.sendSMS(user.getU_phone());
+		/*System.out.println(sms.getNum());*/
+		return String.valueOf(sms.getNum());
 	}
 }
